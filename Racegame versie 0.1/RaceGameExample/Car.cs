a91fb6688a9dd3bfc1d0b2284a2ddb59bd73e4da
Sven Mark Hofstra <hofs1600.student.nhl.nl>
2016-10-27 12:15:38 +0200

    Remove ! mark

--- Racegame versie 0.1/RaceGameExample/Car.cs
+++ Racegame versie 0.1/RaceGameExample/Car.cs
@@ -132,8 +132,8 @@ namespace RaceGameExample {
         public void calculateNewPosition()
         {
             changeSpeed();
-            position.X += (int)Math.Round(speed * Math.Cos(rotation)); //pure magic here!
-            position.Y += (int)Math.Round(speed * Math.Sin(rotation)); //more magic here!
+            position.X += (int)Math.Round(speed * Math.Cos(rotation)); //pure magic here
+            position.Y += (int)Math.Round(speed * Math.Sin(rotation)); //more magic here
         }
     }
 }
