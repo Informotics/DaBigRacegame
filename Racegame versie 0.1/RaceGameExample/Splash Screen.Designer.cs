71d6fcdf83f488004019fe93ad477d0cf35d9e38
kevinsnijder <kevinsnijder9@hotmail.com>
2016-10-30 15:40:08 +0100

    Halfgare splash screen toegevoegd+bugfixes

--- Racegame versie 0.1/RaceGameExample/Splash Screen.Designer.cs
+++ Racegame versie 0.1/RaceGameExample/Splash Screen.Designer.cs
@@ -1,5 +1,7 @@
-﻿namespace RaceGameExample {
-    partial class formRaceGame {
+﻿namespace RaceGameExample
+{
+    partial class Splash_Screen
+    {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -9,8 +11,10 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing) {
-            if (disposing && (components != null)) {
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -22,35 +26,42 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
-            this.timerGameTicks = new System.Windows.Forms.Timer(this.components);
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // timerGameTicks
+            // button1
             // 
-            this.timerGameTicks.Enabled = true;
-            this.timerGameTicks.Interval = 1;
-            this.timerGameTicks.Tick += new System.EventHandler(this.timerGameTicks_Tick);
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(712, 462);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(266, 142);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Lets go!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // formRaceGame
+            // Splash_Screen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::RaceGameExample.Properties.Resources.Racebaan1;
+            this.BackgroundImage = global::RaceGameExample.Properties.Resources.Splash;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.button1);
             this.MaximumSize = new System.Drawing.Size(1024, 768);
             this.MinimumSize = new System.Drawing.Size(1024, 768);
-            this.Name = "formRaceGame";
+            this.Name = "Splash_Screen";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "RaceGameExample";
+            this.Text = "Start";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Timer timerGameTicks;
+        private System.Windows.Forms.Button button1;
     }
 }
-
---
+++ Racegame versie 0.1/RaceGameExample/Splash Screen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RaceGameExample
+{
+    public partial class Splash_Screen : Form
+    {
+        public Splash_Screen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            var myForm = new formRaceGame();
+            myForm.Show();
+
+        }
+    }
+}
