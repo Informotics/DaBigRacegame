f3f7a9e2b4489049b626d0c7a026d87c7a3c4218
holw1100 <holw1100@student.nhl.nl>
2016-10-31 23:35:05 +0100

    Klikwerkend

---
+++ Racegame versie 0.1/RaceGameExample/Sounds.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Media;
+using RaceGameExample.Properties;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RaceGameExample
+{
+    public static class Sounds
+    {
+        public static SoundPlayer Klik = new SoundPlayer(Resources.Click1);
+
+        public static void loadSounds()
+        {
+            Klik.Load();
+        }
+    }
+}
