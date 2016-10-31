e97c2b9109b672d811055639de12c912e6325d26
Rendall Schijven <rendall.schijven@hotmail.com>
2016-10-31 13:42:10 +0100

    Turning?

---
+++ Racegame versie 0.1/RaceGameExample/Audio.cs
@@ -0,0 +1,21 @@
+﻿using System.Media;
+
+namespace RaceGame
+{
+    /// <summary>
+    /// Class waar de audiofiles zich in bevinden.
+    /// </summary>
+    public static class AudioFiles
+    {
+        public static SoundPlayer Click1 = new SoundPlayer("Click1.wav");
+
+        public static bool IsCrashing = false;
+
+        public static void loadSounds()
+        {
+            Click1.Load();
+
+        }
+    }
+}
+
