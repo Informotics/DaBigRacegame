231088d09e0a02d8fd748d301d783c1e66973990
Rendall Schijven <rendall.schijven@hotmail.com>
2016-11-01 15:23:44 +0100

    Fuel en pit

--- Racegame versie 0.1/RaceGameExample/Car.cs
+++ Racegame versie 0.1/RaceGameExample/Car.cs
@@ -13,15 +13,17 @@ namespace RaceGameExample {
         private bool leftPressed = false, rightPressed = false, throttlePressed = false, brakePressed = false;
         private Keys leftKey, rightKey, throttleKey, brakeKey;
         private Image image;
-        private float fuel = 90;
+        public float fuel = 90;
         public int laps = 1;
-        private int checkpoints = 0;
         private int Map;
         const float rotateAmount = 3.0f;
         public bool check1 = false;
         public bool check2 = false;
         public bool check3 = false;
         public bool finish = false;
+        public bool OutFuel = false;
+        public bool pit = false;
+        public int pitCount = 0;
 
         /// <summary>
         /// Constructor of the car class
@@ -87,22 +89,18 @@ namespace RaceGameExample {
             return image;
         }
 
-        private void AmountFuel ()
+        public void AmountFuel ()
         {
             if (speed != 0)
             {
-                fuel -= 0.4f;
+                fuel -= 0.069f;
             }
 
             if (fuel <= 0)
             {
+                coast();
+                OutFuel = true;
                 fuel = 0;
-                if (speed >= .02)
-                    speed -= .05;
-                else if (speed <= -.02)
-                    speed += 0.05;
-                else
-                    speed = 0;
             }
         }
 
@@ -111,52 +109,78 @@ namespace RaceGameExample {
             Map = ChooseMap.Map;
             if (Map == 1)
             {
-                if (getPosition().X > 250 && getPosition().X < 370 && getPosition().Y > 150 && getPosition().Y < 200)
+                if (getPosition().X > 124 && getPosition().X < 360 && getPosition().Y > 510 && getPosition().Y < 620)
                 {
-                    check1 = true;
-                }
-                if (getPosition().X > 900 && getPosition().X < 1010 && getPosition().Y > 560 && getPosition().Y < 620)
-                {
-                    check2 = true;
-                }
-                if (getPosition().Y > 310 && getPosition().Y < 430 && getPosition().X > 540 && getPosition().X < 580)
-                {
-                    check3 = true;
-                }
-                if (getPosition().Y > 540 && getPosition().Y < 745 && getPosition().X > 210 && getPosition().X < 260 && check2)
-                {
-                    finish = true;
-                }
-                if (check1 && check2 && check3 && finish)
-                {
-                    check1 = false;
-                    check2 = false;
-                    check3 = false;
-                    finish = false;
-                    laps++;
-                    if (laps == 4)
+                    pit = true;
+                    fuel = 90;
+                    OutFuel = false;
+                    speed = speed + .1;
+                    if (speed >= 1.5)
                     {
-                        MessageBox.Show("you win");
+                        speed = 1.5;
                     }
+
                 }
+                    if (pit)
+                    {
+                        pit = false;
+                        pitCount++;
+                    }
+
+                    if (getPosition().X > 250 && getPosition().X < 370 && getPosition().Y > 150 && getPosition().Y < 200)
+                    {
+                        check1 = true;
+                    }
+                    if (getPosition().X > 900 && getPosition().X < 1010 && getPosition().Y > 560 && getPosition().Y < 620)
+                    {
+                        check2 = true;
+                    }
+                    if (getPosition().Y > 310 && getPosition().Y < 430 && getPosition().X > 540 && getPosition().X < 580)
+                    {
+                        check3 = true;
+                    }
+                    if (getPosition().Y > 540 && getPosition().Y < 745 && getPosition().X > 210 && getPosition().X < 260 && check2)
+                    {
+                        finish = true;
+                    }
+                    if (check1 && check2 && check3 && finish)
+                    {
+                        check1 = false;
+                        check2 = false;
+                        check3 = false;
+                        finish = false;
+                        laps++;
+                        if (laps == 4 && pitCount > 0)
+                        {
+                            MessageBox.Show("you win");
+                        }
+                    }
             }
 
         }
 
         private void accelerate() 
         {
-            speed = speed + .1;
-
-            if (speed >= 5.0)
-                speed = 5.0;
+            if (OutFuel == false){
+                    speed = speed + .1;
+                    if (speed >= 5.0)
+                    {
+                        speed = 5.0;
+                    }
+                }         
         }
 
         private void brake() 
         {
-            speed = speed - .1;
+            if (OutFuel == false && pit == false)
+            {
+                speed = speed - .1;
 
-            if (speed <= -2.0)
-                speed = -2.0;
+                if (speed <= -2.0)
+                {
+                    speed = -2.0;
+                }
+            }
         }
 
         private void coast()
