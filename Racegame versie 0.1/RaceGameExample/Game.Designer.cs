9d829abc2a534c4e0cc04deda3b1ae8051b3f095
kevinsnijder <kevinsnijder9@hotmail.com>
2016-11-02 14:22:06 +0100

    wtf is happening

--- Racegame versie 0.1/RaceGameExample/Game.Designer.cs
+++ Racegame versie 0.1/RaceGameExample/Game.Designer.cs
@@ -41,28 +41,26 @@
             this.eendgif.BackgroundImage = global::RaceGameExample.Properties.Resources.Eend;
             this.eendgif.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.eendgif.Image = global::RaceGameExample.Properties.Resources.Eend;
-            this.eendgif.Location = new System.Drawing.Point(608, 225);
-            this.eendgif.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.eendgif.Location = new System.Drawing.Point(458, 191);
             this.eendgif.Name = "eendgif";
-            this.eendgif.Size = new System.Drawing.Size(267, 100);
+            this.eendgif.Size = new System.Drawing.Size(200, 81);
             this.eendgif.TabIndex = 0;
             this.eendgif.TabStop = false;
             // 
             // formRaceGame
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::RaceGameExample.Properties.Resources.Racebaan;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1341, 887);
+            this.ClientSize = new System.Drawing.Size(1024, 768);
             this.Controls.Add(this.eendgif);
             this.DoubleBuffered = true;
             this.ForeColor = System.Drawing.Color.ForestGreen;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.MaximumSize = new System.Drawing.Size(1359, 934);
-            this.MinimumSize = new System.Drawing.Size(1359, 934);
+            this.MaximumSize = new System.Drawing.Size(1024, 768);
+            this.MinimumSize = new System.Drawing.Size(1024, 768);
             this.Name = "formRaceGame";
             this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
